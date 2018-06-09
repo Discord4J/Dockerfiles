a4f31fe28e27a7496f0592fbc12520461f6698e1
austinv11 <austinv11@gmail.com>
2018-06-08 22:25:01 -0400

    Adds postgres image (requires some manual configuration)

--- build-images.sh
+++ build-images.sh
@@ -9,3 +9,8 @@ cd ../nginx/
 docker build -t nginx .
 docker tag nginx austinv11/discord4j:nginx
 docker push austinv11/discord4j:nginx
+
+cd ../database/
+docker build -t database .
+docker tag database austinv11/discord4j:database
+docker push austinv11/discord4j:database
