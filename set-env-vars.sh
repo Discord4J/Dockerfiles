d94e9c891affb62bc6816670f3a5465a6d692b9f
austinv11 <austinv11@gmail.com>
2018-06-10 08:51:17 -0400

    Add a wiki container

---
+++ set-env-vars.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+export WIKI_ADMIN_EMAIL='idk@something.com'
+export GOOGLE_CLIENT_ID=''
+export GOOGLE_CLIENT_SECRET=''
+export MICROSOFT_CLIENT_ID=''
+export MICROSOFT_CLIENT_SECRET=''
+export GITHUB_CLIENT_ID=''
+export GITHUB_CLIENT_SECRET=''
+export SESSION_SECRET=''
+export GIT_USER_NAME=''
+export GIT_PASSWORD=''
+export GIT_EMAIL=''
