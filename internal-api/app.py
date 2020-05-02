e62452c8ab4875e7e8c93766e7e4c68a1245e1b0
austinv11 <austinv11@gmail.com>
2020-05-02 19:46:46 -0400

    Improve speed of the /versions endpoint

--- internal-api/app.py
+++ internal-api/app.py
@@ -1,18 +1,27 @@
 import os
 
 from flask import Flask, jsonify
+from flask_caching import Cache
 from github import Github
 
+config = {
+    "CACHE_TYPE": "simple", # Flask-Caching related configs
+    "CACHE_DEFAULT_TIMEOUT": 300
+}
+
 app = Flask(__name__)
+app.config.from_mapping(config)
+cache = Cache(app)
 
 github = Github(os.environ.get('ACCESS_TOKEN'))
 
 
 @app.route("/versions", methods=["GET"])
+@cache.cached(timeout=3*60*60)  # 3 hours in seconds
 def handle():
     repo = github.get_repo("Discord4J/Discord4J")
     releases = list()
-    for release in repo.get_releases():
+    for release in repo.get_releases().get_page(0):  # It's a paginated api, so lets use just the first page
         releases.append({
             "tag": release.tag_name,
             "title": release.title,
